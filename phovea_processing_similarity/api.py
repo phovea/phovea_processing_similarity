a0f9ba1acd606d0e9b4b004ea860ad7d1c5177f0
Klaus Eckelt <Klaus.Eckelt@gmail.com>
2017-10-12 16:47:08 +0200

    Add existing similarity task files from StratomeX

---
+++ phovea_processing_similarity/api.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+from phovea_server.ns import Namespace
+
+
+app = Namespace(__name__)
+
+
+@app.route('/add/<x>/<y>', methods=['GET'])
+def add(x, y):
+  from . import tasks
+  res = tasks.add.delay(x, y)
+  return res.id
+
+
+@app.route('/similarity/<method>/', methods=['GET'])
+def calc_similarity(method):
+  from . import tasks  # import from current package
+  from flask import request
+
+  res = tasks.similarity.delay(method, request.args['range'])
+  return res.id
+
+
+def create():
+  """
+   entry point of this plugin
+  """
+  return app
