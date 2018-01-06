d49e7cee83071b76cf4e83960fc00bfdc0e078fd
Klaus Eckelt <Klaus.Eckelt@gmail.com>
2018-01-06 13:41:37 +0100

    Define route for column similarity #4

--- phovea_processing_similarity/api.py
+++ phovea_processing_similarity/api.py
@@ -1,15 +1,19 @@
 from __future__ import absolute_import
 from phovea_server.ns import Namespace
-
+from . import tasks  # import from current package
+from flask import request
 
 app = Namespace(__name__)
 
 
+@app.route('/column/<method>/', methods=['GET'])
+def calc_similarity(method):
+  res = tasks.column_similarity.delay(method, request.args['range'])
+  return res.id
+
+
 @app.route('/group/<method>/', methods=['GET'])
 def calc_similarity(method):
-  from . import tasks  # import from current package
-  from flask import request
-
   res = tasks.group_similarity.delay(method, request.args['range'])
   return res.id
 
